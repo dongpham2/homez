30f0ba423ec02950dcb91f3791b249be99f843ce
dongpham2 <dongpham21082001@gmail.com>
2024-04-23 19:44:35 +0700

    create update list api

--- api/controllers/list.controller.js
+++ api/controllers/list.controller.js
@@ -29,13 +29,32 @@ export const deleteList = async (req, res, next) => {
     return next(errorHandler(404, "Listing not found!"));
   }
 
-  if (req.user.id !== list.userRef) {
+  if (req.user.id !== List.userRef) {
     return next(errorHandler(401, "You can only delete your own listings!"));
   }
 
   try {
     await List.findByIdAndDelete(req.params.id);
     res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateList = async (req, res, next) => {
+  const list = await List.findById(req.params.id);
+  if (!list) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== list.userRef) {
+    return next(errorHandler(401, "You can only update your own listings!"));
+  }
+
+  try {
+    const updatedList = await List.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedList);
   } catch (error) {
     next(error);
   }
