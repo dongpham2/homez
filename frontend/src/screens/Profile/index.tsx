40cd66a241233482a897159307c89cdfa2caf0e6
imhan14 <quanghannguyen14@gmail.com>
2024-04-25 13:18:39 +0700

    profile UI

--- frontend/src/screens/Profile/index.tsx
+++ frontend/src/screens/Profile/index.tsx
@@ -3,11 +3,12 @@ import { Button } from '../../components/Button'
 import { Input } from '~/components/Input'
 import { useForm } from 'react-hook-form'
 import cameraIcon from '~/assets/icons/camera.icon.svg'
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { cva } from 'class-variance-authority'
 import { cn } from '~/__generated__/utils'
+import { type IUser } from '~/types/user.type'
 
-const settingVariant = cva('h-1 w-full', {
+const tabVariant = cva('h-1 w-full', {
   variants: {
     variant: {
       default: 'bg-transparent',
@@ -20,17 +21,28 @@ const settingVariant = cva('h-1 w-full', {
 })
 
 const Profile = () => {
-  const img = 'https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/anh-dep-thien-nhien-3d-001.jpg'
-  const [isEdit, setIsEdit] = useState(true)
-  const [isSetting, setIsSetting] = useState(false)
+  const img = ''
+  const [toggleState, setToggleState] = useState(1)
+  const [imageUrl, setImageUrl] = useState('')
   const form = useForm()
-  const handleEdit = () => {
-    setIsEdit(true)
-    setIsSetting(false)
+
+  const toggleTab = (index: number) => {
+    setToggleState(index)
   }
-  const handleSetting = () => {
-    setIsEdit(false)
-    setIsSetting(true)
+
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    const reader = new FileReader()
+
+    reader.onload = (e) => {
+      if (e.target) {
+        setImageUrl(e.target.result as string)
+      }
+    }
+
+    if (file) {
+      reader.readAsDataURL(file)
+    }
   }
   return (
     <div className="flex h-screen w-full items-center justify-center bg-[--gray-secondary]">
@@ -40,35 +52,59 @@ const Profile = () => {
           {/* setting bar */}
           <div className="flex gap-4">
             <div>
-              <h4 className="cursor-pointer text-base" onClick={handleEdit}>
+              <h4 className="cursor-pointer text-base" onClick={() => toggleTab(1)}>
                 Chỉnh sửa thông tin
               </h4>
-              <div className={cn(settingVariant({ variant: isEdit ? 'active' : 'default' }))}></div>
+              <div className={cn(tabVariant({ variant: toggleState === 1 ? 'active' : 'default' }))}></div>
             </div>
             <div>
-              <h4 className="cursor-pointer text-base" onClick={handleSetting}>
+              <h4 className="cursor-pointer text-base" onClick={() => toggleTab(2)}>
                 Cài đặt tài khoản
               </h4>
-              <div className={cn(settingVariant({ variant: isSetting ? 'active' : 'default' }))}></div>
+              <div className={cn(tabVariant({ variant: toggleState === 2 ? 'active' : 'default' }))}></div>
             </div>
           </div>
           <div className="mb-5 w-full bg-gray-300 pb-[0.5px]"></div>
           {/* personal infor */}
-          {isEdit && (
+          {toggleState === 1 && (
             <div>
               <div className="flex-col items-center justify-center pb-5">
                 <h2 className="text-lg font-medium sm:text-xl">Thông tin cá nhân</h2>
                 <div className="flex h-60 items-center justify-center">
-                  <Button variant="default">
-                    {img ? (
-                      <img className="flex h-32 w-32 flex-col items-center justify-center rounded-full" src={img} />
-                    ) : (
-                      <div className="flex h-32 w-32 flex-col items-center justify-center rounded-full bg-white">
-                        <img src={cameraIcon} alt="camera" />
-                        <span>Tải ảnh</span>
-                      </div>
-                    )}
-                  </Button>
+                  {
+                    <div className="absolute flex h-32 w-32 flex-col items-center justify-center rounded-full bg-[--gray-secondary]">
+                      {imageUrl ? (
+                        <img src={imageUrl} alt="preview" className="h-full w-full rounded-full object-cover" />
+                      ) : img ? (
+                        <img src={img} alt="preview" className="h-full w-full rounded-full object-cover" />
+                      ) : (
+                        <div className="flex flex-col items-center">
+                          <img src={cameraIcon} alt="preview" />
+                          <span>Tải ảnh</span>
+                        </div>
+                      )}
+                    </div>
+                  }
+                  <FormField
+                    name="avatar"
+                    control={form.control}
+                    render={({ field }) => {
+                      return (
+                        <FormItem>
+                          <FormControl>
+                            <Input
+                              type="file"
+                              placeholder="Tải ảnh"
+                              className="h-32 w-32 cursor-pointer rounded-full opacity-0"
+                              {...{ ...field, onChange: handleImageChange }}
+                            />
+                          </FormControl>
+                          <FormDescription />
+                          <FormMessage />
+                        </FormItem>
+                      )
+                    }}
+                  />
                 </div>
                 <div>
                   <h3 className="pb-2">Họ và tên</h3>
@@ -79,7 +115,7 @@ const Profile = () => {
                       return (
                         <FormItem>
                           <FormControl>
-                            <Input type="fullName" placeholder="Nguyễn Văn A" {...field} />
+                            <Input placeholder="Nguyễn Văn A" {...field} />
                           </FormControl>
                           <FormDescription />
                           <FormMessage />
@@ -101,7 +137,7 @@ const Profile = () => {
                       return (
                         <FormItem>
                           <FormControl>
-                            <Input type="phone" placeholder="0123456789" {...field} />
+                            <Input placeholder="0123456789" {...field} />
                           </FormControl>
                           <FormDescription />
                           <FormMessage />
@@ -129,7 +165,7 @@ const Profile = () => {
             </div>
           )}
           {/* Account Settings */}
-          {isSetting && <div>Account Settings</div>}
+          {toggleState === 2 && <div>Account Settings</div>}
         </form>
       </Form>
     </div>
