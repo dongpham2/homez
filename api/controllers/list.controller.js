e0f798feb0880a77706e7dc64f8f238ab83baa1b
dongpham2 <dongpham21082001@gmail.com>
2024-04-26 13:44:08 +0700

    search api

--- api/controllers/list.controller.js
+++ api/controllers/list.controller.js
@@ -62,53 +62,21 @@ export const updateList = async (req, res, next) => {
 
 
 
-// export const getLists = async (req, res, next) => {
-//   try {
-//     const limit = parseInt(req.query.limit) || 9;
-//     const startIndex = parseInt(req.query.startIndex) || 0;
-//     let offer = req.query.offer;
+export const getLists = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
 
-//     if (offer === undefined || offer === "false") {
-//       offer = { $in: [false, true] };
-//     }
+    const searchTerm = req.query.searchTerm || "";
 
-//     let furnished = req.query.furnished;
+    const lists = await List.find({
+      name: { $regex: searchTerm, $options: "i" },
+    })
+      .limit(limit)
+      .skip(startIndex);
 
-//     if (furnished === undefined || furnished === "false") {
-//       furnished = { $in: [false, true] };
-//     }
-
-//     let parking = req.query.parking;
-
-//     if (parking === undefined || parking === "false") {
-//       parking = { $in: [false, true] };
-//     }
-
-//     let type = req.query.type;
-
-//     if (type === undefined || type === "all") {
-//       type = { $in: ["sale", "rent"] };
-//     }
-
-//     const searchTerm = req.query.searchTerm || "";
-
-//     const sort = req.query.sort || "createdAt";
-
-//     const order = req.query.order || "desc";
-
-//     const lists = await List.find({
-//       name: { $regex: searchTerm, $options: "i" },
-//       offer,
-//       furnished,
-//       parking,
-//       type,
-//     })
-//       .sort({ [sort]: order })
-//       .limit(limit)
-//       .skip(startIndex);
-
-//     return res.status(200).json(lists);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+    return res.status(200).json(lists);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/list.route.js
+++ api/routes/list.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import verifyToken from "../utils/verifyUser.js";
-import { createList, deleteList, getList, updateList } from "../controllers/list.controller.js";
+import { createList, deleteList, getList, getLists, updateList } from "../controllers/list.controller.js";
 
 const router = express.Router();
 
@@ -8,6 +8,6 @@ router.post("/create", verifyToken, createList);
 router.delete("/delete/:id", verifyToken, deleteList);
 router.post("/update/:id", verifyToken, updateList);
 router.get("/get/:id", getList);
-// router.get("/get", getLists);
+router.get("/get", getLists);
 
 export default router;
